c3377cded55056c47cc81040dbc0b11c3a3fcf3e
Willsf2015 <156726395+Willsf2015@users.noreply.github.com>
2024-03-05 13:13:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ let buttonClose = document.querySelector(".button-close");
 let mobileMenu = document.querySelector(".mobile-menu");
 
 function menuShow() {
-  mobileMenu.style.display = "block";
+  mobileMenu.style.height = "250px";
+
   buttonClose.style.display = "block";
   button.style.display = "none";
 }
 
 function menuClose() {
-  mobileMenu.style.display = "none";
+  mobileMenu.style.height = "0";
   buttonClose.style.display = "none";
   button.style.display = "block";
 }
